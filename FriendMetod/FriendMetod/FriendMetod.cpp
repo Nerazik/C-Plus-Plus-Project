acb9348c1416df6854da0ece2cfe134317d48983
Redl <nerazik2018@mail.ru>
2022-02-03 18:37:41 +0300

    GG

--- FriendMetod/FriendMetod/FriendMetod.cpp
+++ FriendMetod/FriendMetod/FriendMetod.cpp
@@ -1,38 +1,68 @@
 ﻿#include <iostream>
 #include <Windows.h>
-#include <conio.h>
-class charlocus
+using namespace std;
+
+class Point
 {
-	int x, y;
-	char cc;
-	void gotoxy(int xpos, int ypos)
-	{
-		COORD scrn;
-		HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-		scrn.X = xpos; scrn.Y = ypos;
-		SetConsoleCursorPosition(hOutput, scrn);
+	friend class Line;
+	friend class Triangel;
+public:
+	Point(int x, int y) {
+		this->x = x;
+		this->y = y;
 	}
-	friend void friend_put(charlocus*, char);
-	public:
-		charlocus(int xi, int yi, char ci)
-
-		{
-			 x = xi; y = yi; cc = ci;
-		}
-
-		void display()
-		{
-			gotoxy(x, y);
-			_putch(cc);
-		};
+	static void gotoxy(int xpos, int ypos)
+	{
+		COORD position;
+		HANDLE screen = GetStdHandle(STD_OUTPUT_HANDLE);
+		position.X = xpos;
+		position.Y = ypos;
+		SetConsoleCursorPosition(screen, position);
+	}
+	
+private:
+	int x, y;
 };
-	
 
-void main()
+class Line
 {
-	charlocus D(21,5, '.');
-	charlocus S(11,11, '.');
-	D.display(); _getch();
-	S.display(); _getch();
-	
+public:
+	Line(Point& a, Point& b) {
+		Point::gotoxy(a.x, a.y);
+		putchar('.');
+		Point::gotoxy(b.x, b.y);
+		putchar('.');
+	}
+};
+
+class Triangel
+{
+public:
+	Triangel(Point& a, Point& b, Point& c) {
+		Point::gotoxy(a.x, a.y);
+		putchar('.');
+		Point::gotoxy(b.x, b.y);
+		putchar('.');
+		Point::gotoxy(c.x, c.y);
+		putchar('.');
+	};
+};
+
+int main()
+{
+	Point A(50, 18);
+	Point B(37, 5);
+	Point C(29, 11);
+	Triangel(A, B, C);
+	Sleep(3000);
+	system("cls");
+	Line(A, B);
+	Sleep(3000);
+	system("cls");
+	Line(A, C);
+	Sleep(3000);
+	system("cls");
+	Line(B, C);
+	Sleep(3000);
+	system("cls");
 }
